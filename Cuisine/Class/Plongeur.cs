52304645c0d777cb5730676fe1aaf18133193e65
Falcort <falcort.jeux@gmail.com>
2018-12-06 10:58:54 +0100

    Specification formelle Plongeur

--- Cuisine/Class/Plongeur.cs
+++ Cuisine/Class/Plongeur.cs
@@ -6,12 +6,20 @@ using System.Text;
 
 public class Plongeur : PersonnelCuisine {
 
+    /// <summary>
+    /// Singleton
+    /// retourne l'instance si elle est deja crée
+    /// sinon crée un instance et la retourne
+    /// </summary>
     public Plongeur() {
     }
 
     private static Plongeur Instance;
 
 
+    /// <summary>
+    /// Retourne l'unique instance
+    /// </summary>
     public void GetInstance() {
         // TODO implement here
     }
