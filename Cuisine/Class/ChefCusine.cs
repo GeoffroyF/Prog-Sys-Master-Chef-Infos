e7b1bbee75d6f34b525a92583fc51a89d14f8f51
Falcort <falcort.jeux@gmail.com>
2018-12-06 10:50:29 +0100

    Import les classes personnelle de cuisine

---
+++ Cuisine/Class/ChefCusine.cs
@@ -0,0 +1,18 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class ChefCusine : PersonnelCuisine {
+
+    public ChefCusine() {
+    }
+
+    private static ChefCusine Instance;
+
+    public void GetInstance() {
+        // TODO implement here
+    }
+
+}
