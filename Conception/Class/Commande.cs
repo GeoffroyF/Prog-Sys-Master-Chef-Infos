6dfcad5af0b3a0bcf3e4bbf8ce38dabe6437e309
Falcort <falcort.jeux@gmail.com>
2018-12-06 09:11:51 +0100

    Organisation

---
+++ Conception/Class/Commande.cs
@@ -0,0 +1,33 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class Commande {
+
+    public Commande() {
+    }
+
+    private Table Table;
+
+    private String Plat;
+
+    private boolean EnCours;
+
+    public void GetTable() {
+        // TODO implement here
+    }
+
+    public void GetPlats() {
+        // TODO implement here
+    }
+
+    /// <summary>
+    /// @param Plat
+    /// </summary>
+    public void SetPlats(void Plat) {
+        // TODO implement here
+    }
+
+}
---
+++ Conception/Class/CommunicationCuisineSalle.cs
@@ -0,0 +1,37 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class CommunicationCuisineSalle : Observer {
+
+    public CommunicationCuisineSalle() {
+    }
+
+
+
+    public void GetSaleFromCom() {
+        // TODO implement here
+    }
+
+    /// <summary>
+    /// @param Plat
+    /// </summary>
+    public void AddPlatToPassePlat(void Plat) {
+        // TODO implement here
+    }
+
+    /// <summary>
+    /// @param String 
+    /// @param int
+    /// </summary>
+    public void EnvoiePropre(void String, void int) {
+        // TODO implement here
+    }
+
+    public void SendCommande() {
+        // TODO implement here
+    }
+
+}
