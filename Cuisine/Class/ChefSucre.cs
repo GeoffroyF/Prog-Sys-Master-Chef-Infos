31392033791ea6d813fc3630e4dcf1848763879e
Falcort <falcort.jeux@gmail.com>
2018-12-06 10:59:44 +0100

    Specification formelle ChefSucre

--- Cuisine/Class/ChefSucre.cs
+++ Cuisine/Class/ChefSucre.cs
@@ -6,11 +6,19 @@ using System.Text;
 
 public class ChefSucre : PersonnelCuisine {
 
+    /// <summary>
+    /// Singleton
+    /// retourne l'instance si elle est deja crée
+    /// sinon crée un instance et la retourne
+    /// </summary>
     public ChefSucre() {
     }
 
     private static ChefSucre Instance;
 
+    /// <summary>
+    /// Retourne l'unique instance
+    /// </summary>
     public void GetInstance() {
         // TODO implement here
     }
