39c6bc2053ba19a6fd606802941ead19bfd9e5eb
Falcort <falcort.jeux@gmail.com>
2018-12-06 11:00:17 +0100

    Specification formelle ChefSale

--- Cuisine/Class/ChefSale.cs
+++ Cuisine/Class/ChefSale.cs
@@ -6,11 +6,19 @@ using System.Text;
 
 public class ChefSale : PersonnelCuisine {
 
+    /// <summary>
+    /// Singleton
+    /// retourne l'instance si elle est deja crée
+    /// sinon crée un instance et la retourne
+    /// </summary>
     public ChefSale() {
     }
 
     private static ChefSale Instance;
 
+    /// <summary>
+    /// Retourne l'unique instance
+    /// </summary>
     public void GetInstance() {
         // TODO implement here
     }
