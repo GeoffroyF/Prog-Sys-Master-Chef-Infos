bc584957255cdb36e8ee0a6e502425a8a0ef2124
vinyljungler <33051275+vinyljungler@users.noreply.github.com>
2018-12-06 10:52:25 +0100

    Ajout des differentes Classes dans le projet
    les classes restent vides

---
+++ Salle/Class/Salle.cs
@@ -0,0 +1,45 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class Salle : PresentoireSalle {
+
+    public Salle() {
+    }
+
+    private Carre CarreUn;
+
+    private Carre CarreDeux;
+
+    private MaitreHotel MaitreHotel;
+
+    private CommisSalle Commis;
+
+    private static Salle Instance;
+
+    private PresentoireSalle PresentoireSalle;
+
+
+    public void GetCarreUn() {
+        // TODO implement here
+    }
+
+    public void GetCarreDeux() {
+        // TODO implement here
+    }
+
+    public void GetMaitreHotel() {
+        // TODO implement here
+    }
+
+    public void GetCommis() {
+        // TODO implement here
+    }
+
+    public void GetInstance() {
+        // TODO implement here
+    }
+
+}
