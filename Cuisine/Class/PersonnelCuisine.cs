6ea2d1570776ffa90393a8a192057e5122b32906
Falcort <falcort.jeux@gmail.com>
2018-12-06 10:56:46 +0100

    Specification formelle PersonnelCuisine

--- Cuisine/Class/PersonnelCuisine.cs
+++ Cuisine/Class/PersonnelCuisine.cs
@@ -6,6 +6,15 @@ using System.Text;
 
 public class PersonnelCuisine {
 
+    /// <summary>
+    /// Crée un personne de cuisine
+    /// ilt faut un deux arguments
+    ///  une string pour le nom
+    ///  uns string pour le prenom
+    /// A la création le isBusy doit etre false
+    /// @param String nom
+    /// @param String prenom
+    /// </summary>
     public PersonnelCuisine() {
     }
 
@@ -13,24 +22,35 @@ public class PersonnelCuisine {
 
     private String Prenom;
 
-    private boolean isBusy;
+    private bool isBusy;
 
+
+    /// <summary>
+    /// Retourne le string Nom
+    /// </summary>
     public void GetNom() {
         // TODO implement here
     }
 
+    /// <summary>
+    /// Retourne le string prenom
+    /// </summary>
     public void GetPrenom() {
         // TODO implement here
     }
+
 
     public void GetIsBusy() {
         // TODO implement here
     }
 
     /// <summary>
-    /// @param boolean
+    /// Set le boolean busy a true ou false
+    ///  true : il est actuellement occuper
+    ///  false : il est actuellement libre
+    /// @param bool status
     /// </summary>
-    public void SetIsBusy(void boolean) {
+    public void SetIsBusy(bool) {
         // TODO implement here
     }
 
