7915bf38f9da37249d5fdbcfc7c1de882dd37ce0
Falcort <falcort.jeux@gmail.com>
2018-12-07 09:58:49 +0100

    TDD Machine

--- Cuisine/Class/Machines.cs
+++ Cuisine/Class/Machines.cs
@@ -17,17 +17,33 @@ public class Machines {
 
     private String Nom;
 
-
+    /// <summary>
+    /// Function that make the machine run
+    /// </summary>
     public void Utilise() {
         // TODO implement here
     }
 
+    /// <summary>
+    /// Return the actual person using the machine
+    /// </summary>
     public void GetProprio() {
         // TODO implement here
     }
 
+    /// <summary>
+    /// Return the name of the machine
+    /// </summary>
     public void GetNom() {
         // TODO implement here
+    }
+
+    /// <summary>
+    /// Set the new prorpio of the machine
+    /// </summary>
+    /// <param name="proprio"></param>
+    public void SetProprio(PersonnelCuisine proprio) {
+
     }
 
 }
---
+++ Cuisine/Test/MachinesTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test
+{
+    [TestClass]
+    public class MachineTest
+    {
+        private readonly Machines MachineUn;
+        private readonly Machines MachineDeux;
+
+        public MachineTest()
+        {
+            MachineUn = new Machines();
+            MachineDeux = new Machines();
+        }
+
+        [TestMethod]
+        public void TestSingletonCuisine()
+        {
+            Assert.AreEqual(MachineUn.GetInstance(), MachineDeux.GetInstance(), "La Cuisine n'est pas un singleton");
+        }
+    }
+}
