cfc7c211c8a7e6a4c6a4218ff4aa4a1c654a09f0
Falcort <falcort.jeux@gmail.com>
2018-12-07 09:38:09 +0100

    TDD Stock

---
+++ Cuisine/Test/StockTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Test
+{
+    [TestClass]
+    public class StockTest
+    {
+        private readonly Stock StockUn;
+        private readonly Stock StockDeux;
+
+        public StockTest()
+        {
+            StockUn = new Stock();
+            StockDeux = new Stock();
+        }
+
+        [TestMethod]
+        public void TestSingletonCuisine()
+        {
+            Assert.AreEqual(StockUn.GetInstance(), StockDeux.GetInstance(), "La Cuisine n'est pas un singleton");
+        }
+    }
+}
